f6a3fc3a3190bc2f15c0768004e589e25b9f9b0c
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2020-04-17 08:50:41 +0200

    Added Lowe's ratio test matchers.

--- local_feature_evaluation/matchers.py
+++ local_feature_evaluation/matchers.py
@@ -11,3 +11,66 @@ def mutual_nn_matcher(descriptors1, descriptors2):
     mask = ids1 == nn21[nn12]
     matches = torch.stack([ids1[mask], nn12[mask]]).t()
     return matches.data.cpu().numpy()
+
+
+# Symmetric Lowe's ratio test matcher for L2 normalized descriptors.
+def ratio_matcher(descriptors1, descriptors2, ratio=0.8):
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+
+    # Retrieve top 2 nearest neighbors 1->2.
+    nns_sim, nns = torch.topk(sim, 2, dim=1)
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    # Compute Lowe's ratio.
+    ratios12 = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    # Save first NN.
+    nn12 = nns[:, 0]
+
+    # Retrieve top 2 nearest neighbors 1->2.
+    nns_sim, nns = torch.topk(sim.t(), 2, dim=1)
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    # Compute Lowe's ratio.
+    ratios21 = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    # Save first NN.
+    nn21 = nns[:, 0]
+    
+    # Symmetric ratio test.
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    mask = torch.min(ratios12 <= ratio, ratios21[nn12] <= ratio)
+    
+    # Final matches.
+    matches = torch.stack([ids1[mask], nn12[mask]], dim=-1)
+
+    return matches.data.cpu().numpy()
+
+
+# Mutual NN + symmetric Lowe's ratio test matcher for L2 normalized descriptors.
+def mutual_nn_ratio_matcher(descriptors1, descriptors2, ratio=0.8):
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+
+    # Retrieve top 2 nearest neighbors 1->2.
+    nns_sim, nns = torch.topk(sim, 2, dim=1)
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    # Compute Lowe's ratio.
+    ratios12 = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    # Save first NN and match similarity.
+    nn12 = nns[:, 0]
+
+    # Retrieve top 2 nearest neighbors 1->2.
+    nns_sim, nns = torch.topk(sim.t(), 2, dim=1)
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    # Compute Lowe's ratio.
+    ratios21 = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    # Save first NN.
+    nn21 = nns[:, 0]
+    
+    # Mutual NN + symmetric ratio test.
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    mask = torch.min(ids1 == nn21[nn12], torch.min(ratios12 <= ratio, ratios21[nn12] <= ratio))
+    
+    # Final matches.
+    matches = torch.stack([ids1[mask], nn12[mask]], dim=-1)
+
+    return matches.data.cpu().numpy()
+
